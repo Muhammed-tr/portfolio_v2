605571579f1af2ffdc4fcdc3856018d84717b91a
Muhammedİde <muhammeddemir@papilon.com.tr>
2024-08-27 16:03:30 +0300

    added CV

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,8 +2,11 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import image from "../../Assets/../Assets/MuhammedİzzetDemir_CV.jpg";
+import pdf from "../../Assets/../Assets/Muhammedİzzet_DEMİR_CV.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -19,8 +22,8 @@ function ResumeNew() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={image}
-            download="MuhammedİzzetDemir_CV.jpg"
+            href={pdf}
+            target="_blank"
             style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
@@ -29,23 +32,16 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <img
-            src={image}
-            alt="Resume"
-            style={{
-              maxWidth: "1000px", // Genişlik ayarı
-              height: "auto", // Otomatik yükseklik ayarı
-              boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)", // Hafif gölge efekti
-            }}
-            className="d-flex justify-content-center"
-          />
+          <Document file={pdf} className="d-flex justify-content-center">
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          </Document>
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={image}
-            download="MuhammedİzzetDemir_CV.jpg"
+            href={pdf}
+            target="_blank"
             style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
